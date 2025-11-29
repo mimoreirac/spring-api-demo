8411be024506fb931fbbaf5f57477c536d618308
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-11-29 17:00:48 -0500

    First commit

---
+++ src/main/java/com/puce/consumorestapi/ConsumoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.puce.consumorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumoRestApiApplication.class, args);
+    }
+
+}
